8bf970aac02216526efc6c26d1ce3b0ef9f67498
weichengwu <wuvcen@gmail.com>
2022-06-13 17:18:25 +0800

    upgrade pub

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:alice/alice.dart';
-import 'package:alice_example/posts_service.dart';
-import 'package:chopper/chopper.dart';
-import 'package:http/http.dart' as http;
 import 'package:alice/core/alice_http_client_extensions.dart';
 import 'package:alice/core/alice_http_extensions.dart';
+import 'package:chopper/chopper.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+
+import 'posts_service.dart';
 
 void main() => runApp(MyApp());
 
